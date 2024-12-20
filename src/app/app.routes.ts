b4d077fe09d7beca7ab1d79007ef9eb5e687bf8c
Samariddinn <urinovsalohiddin077@gmail.com>
2024-12-20 15:04:48 +0300

    init home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path : 'home', loadComponent: () => import('./pages/home/home.component')},
+    {path: 'not-found', loadComponent: () => import('./shared/components/not-found/not-found.component')},
+    {path: '**', redirectTo: 'home'}
+];  
---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component ({
+    selector: 'app-navbar',
+    templateUrl: 'navbar.component.html',
+    imports: [MatButtonModule]
+
+})
+
+export  class NavbarComponent   {
+
+}
